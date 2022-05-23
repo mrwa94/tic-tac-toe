d05045f1b7f95bbbdacd91e1995ff8c2cc2276d4
mrwa94 <90597166+mrwa94@users.noreply.github.com>
2022-05-23 21:50:33 +0300

    Initial Commit

---
+++ Tic Tac Toe/Tic_Tac_ToeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Tic_Tac_ToeApp.swift
+//  Tic Tac Toe
+//
+//  Created by Ayman alsubhi on 22/10/1443 AH.
+//
+
+import SwiftUI
+
+@main
+struct Tic_Tac_ToeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
